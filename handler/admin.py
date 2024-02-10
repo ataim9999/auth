aac82fcbd300d5a10619aaca2f7868bf009727d8
ataim <daanprive0@gmail.com>
2024-02-10 11:08:58 +0100

    Function create table users if not exists

--- handler/admin.py
+++ handler/admin.py
@@ -3,19 +3,27 @@ import say
 import colors
 import functions
 
+from say import *
 from colors import *
+from functions import *
 
 mydb = functions.connect()
 
 if mydb is not None:
-    choice = input(f"{yellow}[{lightblack}{name}@{ip}{yellow}]{white} > ")
+    print("1. list tables\n2. Create table users")
+    while True:
+        choice = input(f"{yellow}[{lightblack}{name}@{ip}{yellow}]{white} > ")
 
-    if choice == "1":
-        mycursor = mydb.cursor()
+        if choice == "1":
+            mycursor = mydb.cursor()
 
-        mycursor.execute("SHOW TABLES")
+            mycursor.execute("SHOW TABLES")
 
-        for x in mycursor:
-            print(x)
+            for x in mycursor:
+                print(x)
+        elif choice == "2":
+            mycursor = mydb.cursor()
+            TableIfNone(mycursor)
+
 else:
     say.say("Connection failed", "error")
--- handler/functions.py
+++ handler/functions.py
@@ -19,3 +19,9 @@ def connect():
     
 
 
+def TableIfNone(cursor):
+    try:
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), password VARCHAR(50))")
+        say.say("Table created", "success")
+    except mysql.connector.Error as err:
+        say.say(f"Error creating table: {err}", "error")
